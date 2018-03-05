2c5ada4343f835998e6fd85ef160e49182c53e7a
Aubrey Neimeier <neimeier.1@osu.edu>
2018-03-05 18:38:18 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -83,4 +83,16 @@ class Student
       DB[:conn].execute(sql)
   end
 
+  def self.first_X_students_in_grade_10(X)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT ?
+      SQL
+
+      DB[:conn].execute(sql)
+  end
+
+
 end
