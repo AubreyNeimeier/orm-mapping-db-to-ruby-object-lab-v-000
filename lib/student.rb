faf3ded9cb409f6fb3c89923653e8677d8259444
Aubrey Neimeier <neimeier.1@osu.edu>
2018-03-05 18:26:45 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,7 +71,6 @@ def self.count_all_students_in_grade_9
     WHERE grade = 9
     SQL
 
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
+    DB[:conn].execute(sql)
     end
   end
